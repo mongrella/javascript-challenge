6dbdab8a398786e06d8a9f23c24d6d60e248df6e
lucille bluth <melissa.mongrella@northwestern.edu>
2021-05-18 21:30:30 -0500

    added prevent default

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -24,14 +24,15 @@ tableData.forEach(function(ufoSighting) {
 var button = d3.select("#filter-btn");
 button.on("click", function() {
 
-    
+    d3.event.preventDefault();
+        
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
     console.log(inputValue);
     // Filter data
-    var filteredData = tableData.filter(sight => sight.bloodType === inputValue);
+    var filteredData = tableData.filter(sight => sight.datetime === inputValue);
     console.log(filteredData);
 
-}
+});
