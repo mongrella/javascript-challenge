b5c07fcbad8ae221b1026c98bbac1f0b31ecc816
lucille bluth <melissa.mongrella@northwestern.edu>
2021-05-18 19:59:05 -0500

    added table code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,24 +9,14 @@ var tbody = d3.select("tbody");
 // Add columns with UFO sighting data
 tableData.forEach(function(ufoSighting) {
     console.log(ufoSighting);
-    //Append empty rows
+    //Append a row for each value in our data array
     var row = tbody.append("tr");
-    //Use object.entries to grab data
+    //Use object.entries to grab data from the array
     Object.entries(ufoSighting).forEach(function([key, value]) {
         console.log(key, value);
-        //Append cell to rows
+        //Append text and populate table with cell values
         var cell = row.append("td");
         cell.text(value);
     });
 });
-
-
-
-    //tbody.append("tr");
-
-    //for (key in row){
-        //const cell = tbody.append("td");
-        //cell.text(row[key]);
-    //}
-//});
 
