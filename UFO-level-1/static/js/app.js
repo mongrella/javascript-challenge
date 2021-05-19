0cd7332a5ed3fa996e0ee1bf384b97af5cfdcff6
lucille bluth <melissa.mongrella@northwestern.edu>
2021-05-18 19:48:44 -0500

    added empty rows

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,16 +1,32 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // Create references to table body
 var tbody = d3.select("tbody");
 
-// Add columns with UFO sighting data
-tableData.forEach(row => {
-    tbody.append("tr");
 
-    for (key in row){
-        const cell = tbody.append("td");
-        cell.text(row[key]);
-    }
+// Add columns with UFO sighting data
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    //Append empty rows
+    var row = tbody.append("tr");
+    //Use object.entries to grab data
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        //Append cell to rows
+        var cell = row.append("td");
+        cell.text(value);
+    });
 });
+
+
+
+    //tbody.append("tr");
+
+    //for (key in row){
+        //const cell = tbody.append("td");
+        //cell.text(row[key]);
+    //}
+//});
 
