225e66a7a34cdeb91bafd58224e2fcdd4f2c21a3
lucille bluth <melissa.mongrella@northwestern.edu>
2021-05-18 19:30:52 -0500

    started

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Create references to table body
+var tbody = d3.select("tbody");
+
+// Add columns with UFO sighting data
+tableData.forEach(row => {
+    tbody.append("tr");
+
+    for (key in row){
+        const cell = tbody.append("td");
+        cell.text(row[key]);
+    }
+});
+
